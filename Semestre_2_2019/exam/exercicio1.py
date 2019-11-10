284211e74b9bf370500aefeea23d69ebf7c3078f
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-11-10 18:30:03 -0300

    repair 2 exam

--- Semestre_2_2019/exam/exercicio1.py
+++ Semestre_2_2019/exam/exercicio1.py
@@ -1,8 +1,15 @@
-def cadastro(codigo, produto, quantidade, preco):
-    f = open('txt/mercado1.txt', 'a')
+import csv
+
+def cadastro(codigo, produto, quantidade, preco, tipo_do_mercado):
     total = quantidade*preco
-    f.write('%d|'%codigo + '%s|'%produto + '%.2f\n'%total)
-    f.close()
+
+    file_name = 'txt/mercado{}.txt'.format(tipo_do_mercado)
+
+    with open(file_name, 'a', newline='') as csvfile:
+        fieldnames = ['product', 'total_cost']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writerow({'product': produto, 'total_cost': total})
 
 def ler_dados():
     quantidade_lista_compras = int(input("Quantidade na lista: "))
@@ -10,11 +17,14 @@ def ler_dados():
     for x in range(quantidade_lista_compras):
         tipo_mercado = int(input("Tipo do mercado: "))
         codigo = int(input("Codigo: "))
-        produto = str(input("Produto: "))
+        produto = str(input("Produto: ")).lower()
         preco = float(input("Preço: "))
         quantidade_de_produto = int(input("Quantidade: "))
         preco_total = preco*quantidade_de_produto
 
-        cadastro(codigo, produto, quantidade_de_produto, preco)
+        cadastro(codigo, produto, quantidade_de_produto, preco, tipo_mercado)
+
+# def mercado_mais_barato():
+
 
 init_progam = ler_dados()
