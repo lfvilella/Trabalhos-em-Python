b3945ec8327086a68a2f565543d96461d633dcb7
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-12-14 21:34:29 -0300

    testing setuptools

---
+++ head_first/funcoes/README.txt
@@ -0,0 +1,1 @@
+Hi! This is my first experience using the 'setuptools'
---
+++ head_first/funcoes/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch_lfvilella',
+    version='1.0',
+    description='My First Search Tools',
+    author='Luís Felipe Fabro Vilella',
+    author_email='vilella.luisfelipe@gmail.com',
+    url='github.com/lfvilella',
+    py_modules=['vsearch_lfvilella'],
+)
