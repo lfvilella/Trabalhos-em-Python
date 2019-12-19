c24d23584b7b6c030320b48e9748954652bd84ce
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-12-19 19:31:39 -0300

    Organizing and rename the module

--- head_first/basic_methods/funcoes/setup.py
+++ head_first/basic_methods/funcoes/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='vsearch_lfvilella',
+    name='vsearch_lfvilella', # Show in pip freeze
     version='1.0',
     description='My First Search Tools',
     author='Luís Felipe Fabro Vilella',
     author_email='vilella.luisfelipe@gmail.com',
     url='github.com/lfvilella',
-    py_modules=['vsearch'],
+    py_modules=['vsearch_lfvilella'],
 )
