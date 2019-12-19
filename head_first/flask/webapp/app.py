9de9c274928e04be4d0f40670869e25e3f1fcce3
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-12-19 18:34:23 -0300

    adding log

--- head_first/flask/webapp/app.py
+++ head_first/flask/webapp/app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ def do_search():
     phrase = request.form['phrase']
     letters = request.form['letters']
     result = str(search_letters(phrase, letters))
+    log_request(request, result)
     return render_template('results.html',
                             the_title='Here are your results:',
                             the_phrase=phrase,
@@ -22,6 +24,18 @@ def do_search():
 def entry_page():
     return render_template('entry.html',
                             the_title='Welcome to Search For Letters on the Web!')
+
+def log_request(req:'flask_request', res:str) -> None:
+    with open('basic_db/vsearch.log', 'a') as log:
+        print(datetime.datetime.utcnow().isoformat(), req, req.form, res, file=log)
+
+@app.route('/viewlog')
+def view_the_log():
+    with open('basic_db/vsearch.log') as log:
+        contents = log.read() # .read() return all file
+    return contents
+        
+        
 
 # This run like a security to development:
 if __name__ == '__main__':
