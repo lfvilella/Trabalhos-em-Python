10feec9a2d573f9d563a3ca51166defa50698b35
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-12-20 09:33:44 -0300

    import module and repair format on db

--- head_first/flask/webapp/app.py
+++ head_first/flask/webapp/app.py
@@ -1,17 +1,14 @@
 import datetime
+import vsearch_lfvilella
 from flask import Flask, render_template, request, escape
 
 app = Flask(__name__)
 
-
-def search_letters(phrase, letters='aeiou'):
-    return set(letters).intersection(set(phrase))
-
 @app.route('/search4', methods=['POST'])
 def do_search():
-    phrase = request.form['phrase']
+    phrase = (request.form['phrase']).lower()
     letters = request.form['letters']
-    result = str(search_letters(phrase, letters))
+    result = str(vsearch_lfvilella.search_letters(phrase, letters))
     log_request(request, result)
     return render_template('results.html',
                             the_title='Here are your results:',
@@ -27,7 +24,11 @@ def entry_page():
 
 def log_request(req:'flask_request', res:str) -> None:
     with open('basic_db/vsearch.log', 'a') as log:
-        print(datetime.datetime.utcnow().isoformat(), req, req.form, res, file=log)
+        print(datetime.datetime.utcnow().isoformat(),
+            req.form,
+            req.remote_addr,
+            req.user_agent,
+            res, file=log, sep=' | ')
 
 @app.route('/viewlog')
 def view_the_log():
