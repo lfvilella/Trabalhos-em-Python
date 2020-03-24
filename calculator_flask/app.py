73b9197d20638e5476bbf3eca158777d85c7d2c3
Felipe Vilella <vilella.luisfelipe@gmail.com>
2020-03-24 20:18:34 -0300

    put voice on calculator

--- calculator_flask/app.py
+++ calculator_flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import speech_recognition as sr
 
 app = Flask(__name__)
 
@@ -9,9 +10,35 @@ def main(sum=sum):
         return render_template('index.html')
 
     elif request.method == 'POST':
-        expression = request.form.get('textview')
-        sum = eval(expression)
-        return render_template('index.html', sum=sum)
+        try:
+            frase = microphone()
+            sum = eval(frase)
+            return render_template('index.html', sum=sum, frase=frase)
+    
+        except:
+            return render_template('index.html', sum="TryAgain")
+
+    # elif request.method == 'POST':
+    #     expression = request.form.get('textview')
+    #     sum = eval(expression)
+    #     return render_template('index.html', sum=sum)
+
+def microphone():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+    with mic as source:
+        print("Diga alguma coisa: ")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+        try:
+            frase = r.recognize_google(audio,language='pt-BR')
+            # print("Você disse: " + frase)
+            return frase
+
+        except:
+            # print("Não entendi")
+            return "Não entendi"
 
 
 if __name__ == '__main__':
